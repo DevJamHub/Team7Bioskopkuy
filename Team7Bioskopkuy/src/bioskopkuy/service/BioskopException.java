0adc861076c916b4453a9250d6ab8ea382e2e067
DevJam <sigitnovriyanto002@gmail.com>
2025-06-18 18:56:07 +0700

    Baru tgl 18 Juni di jam 18.56

--- Team7Bioskopkuy/src/bioskopkuy/service/BioskopException.java
+++
@@ -1,7 +0,0 @@
-package bioskopkuy.service;
-
-public class BioskopException extends Exception {
-    public BioskopException(String message) {
-        super(message); // Memanggil konstruktor kelas induk (Exception) dengan pesan yang diberikan.
-    }
-}
